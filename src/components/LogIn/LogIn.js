a7434072b6f1491cee71494b7ac3a1737a3fb085
Proloycb <proloychacroborty@gmail.com>
2022-04-10 00:15:50 +0600

    private route setup finished

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,13 +1,29 @@
+import { getAuth } from 'firebase/auth';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import React from 'react';
-import useFirebase from '../../hooks/useFirebase';
+import app from '../../firebase.init';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const auth = getAuth(app);
 
 const LogIn = () => {
-    const {signInWithGoogle} = useFirebase();
+    const [signInWithGoogle] = useSignInWithGoogle(auth);
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const from = location.state?.from?.pathname || '/'
+
+    const handleGoogleSignIn = () => {
+        signInWithGoogle()
+        .then(() => {
+            navigate(from, {replace: true})
+        })
+    }
     return (
         <div>
             <h2>Please Login</h2>
             <div style={{margin: '20px'}}>
-                <button onClick={signInWithGoogle}>Google Sign In</button>
+                <button onClick={handleGoogleSignIn}>Google Sign In</button>
             </div>
             <form>
                 <input type="email" name="" id="" placeholder='Your Email' />
